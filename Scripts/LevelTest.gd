756cfdee13f7b321b655bd243ec14bf57331235c
Avzgui <contact.avzgui@gmail.com>
2021-10-02 15:24:07 +0200

    ajout objet aleatoire

--- Scripts/LevelTest.gd
+++ Scripts/LevelTest.gd
@@ -19,21 +19,24 @@ func _init():
 			break
 		elif !file_name.begins_with("."):
 			#get_next() returns a string so this can be used to load the images into an array.
-			_falling_objects_ressources.append(_falling_objects_path + file_name)
+			_falling_objects_ressources.append(load(_falling_objects_path + file_name))
 	dir.list_dir_end()
 	print (_falling_objects_ressources)
 
+func _ready():
+	randomize()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		pop_falling_object_in_zone(_zone.get_node("Min").global_position, _zone.get_node("Max").global_position)
 
-func instance_falling_object(position : Vector2) -> RigidBody2D :
-	var object = _falling_object_resource.instance()
+func instance_falling_object(id : int, position : Vector2) -> RigidBody2D :
+	var object = _falling_objects_ressources[id].instance()
 	object.position = position
 	return object
 	
 func pop_falling_object_in_zone(position_min : Vector2, position_max : Vector2) -> void:
+	var id = randi() % _falling_objects_ressources.size()
 	var pos = Vector2(rand_range(position_min.x, position_max.x), rand_range(position_min.y, position_max.y))
-	var object = instance_falling_object(pos)
+	var object = instance_falling_object(id, pos)
 	call_deferred("add_child", object)
