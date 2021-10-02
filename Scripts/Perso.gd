85b3e9ea392e1ac2c6f292f030ecdfde8fed4b1a
riffet jérémy <jeremy.riffet@gmail.com>
2021-10-02 10:57:15 +0200

    New perso

---
+++ Scripts/Perso.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export (Vector2) var speed = Vector2(20, 0)
+
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		position += speed * delta
+	elif Input.is_action_pressed("ui_left"):
+		position -= speed * delta
