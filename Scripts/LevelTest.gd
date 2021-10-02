c13335311e983c2d77ace7f0998a94eb100c676d
jajalecapouet <nicolas.jarose@gmail.com>
2021-10-02 15:13:07 +0200

    creation de pop aleatoire d objet

--- Scripts/LevelTest.gd
+++ Scripts/LevelTest.gd
@@ -1,7 +1,28 @@
 extends Node2D
 
+
 var _falling_object_resource = preload("res://Scenes/FallingObjects/Chaise.tscn")
 onready var _zone = $Zone
+
+var _falling_objects_path = "res://Scenes/FallingObjects/"
+var _falling_objects_ressources = []
+
+func _init():
+	var dir = Directory.new()
+	dir.open(_falling_objects_path)
+	
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+			#break the while loop when get_next() returns ""
+			break
+		elif !file_name.begins_with("."):
+			#get_next() returns a string so this can be used to load the images into an array.
+			_falling_objects_ressources.append(_falling_objects_path + file_name)
+	dir.list_dir_end()
+	print (_falling_objects_ressources)
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
