b403861f6914a78836cdcc69206b87b473258056
Avzgui <contact.avzgui@gmail.com>
2021-10-03 14:24:09 +0200

    ajout falling sound

--- Scripts/LevelTest.gd
+++ Scripts/LevelTest.gd
@@ -6,27 +6,13 @@ var update_height = false
 func _ready():
 	_current_height = get_viewport_rect().size.y
 	Signals.connect("height_updated", self, "on_height_updated")
+	$Timer.connect("timeout", self, "on_timer_timeout")
 
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		var falling_object = $Popper.pop_object_with_initial_position()
-		if falling_object != null :
-			call_deferred("_add_falling_object", falling_object[0], falling_object[1])
-			
-#	if event.is_action_pressed("ui_page_up"):
-#		var new_pos = $Popper.position
-#		new_pos.y -= _current_height
-#		_current_height *= 2.0
-#		Signals.emit_signal("popper_height_changed", _current_height)
-#		$Popper.update_height(new_pos)
-#	elif event.is_action_pressed("ui_page_down"):
-#		if _current_height > get_viewport_rect().size.y :
-#			_current_height /= 2.0
-#			var new_pos = $Popper.position
-#			new_pos.y += _current_height
-#			Signals.emit_signal("popper_height_changed", _current_height)
-#			$Popper.update_height(new_pos)
-			
+func on_timer_timeout():
+	var falling_object = $Popper.pop_object_with_initial_position()
+	if falling_object != null :
+		call_deferred("_add_falling_object", falling_object[0], falling_object[1])
+
 func _add_falling_object(falling_object : RigidBody2D, init_global_position : Vector2):
 	add_child(falling_object)
 	falling_object.set_global_position(init_global_position)
