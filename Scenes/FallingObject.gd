f4f6e1a67703cdd691c4d355bdb493aaabd4eaa4
Avzgui <contact.avzgui@gmail.com>
2021-10-04 08:57:24 +0200

    update nuages

--- Scenes/FallingObject.gd
+++ Scenes/FallingObject.gd
@@ -25,6 +25,7 @@ func _physics_process(delta):
 
 func _on_Area2D_body_entered(body):
 	if body != self and self.mode != RigidBody.MODE_RIGID:
+		$AudioStreamPlayer2D.play()
 		self.call_deferred("set_mode", RigidBody.MODE_RIGID)
 	if body is RigidBody2D and tabObjets.size() <20:
 		tabObjets.append(body)
