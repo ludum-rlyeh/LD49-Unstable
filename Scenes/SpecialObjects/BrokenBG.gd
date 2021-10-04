1130060b111652c322de3d90f3aca7d6fd1f6cda
Avzgui <contact.avzgui@gmail.com>
2021-10-04 21:11:57 +0200

    polish and fix error

--- Scenes/SpecialObjects/BrokenBG.gd
+++ Scenes/SpecialObjects/BrokenBG.gd
@@ -7,7 +7,7 @@ func _ready():
 
 func on_game_ended():
 	self.show()
-	_piece_falls(_id)
+	call_deferred("_piece_falls", _id)
 	_id -= 1
 	$BreakTimer.start()
 
@@ -16,14 +16,15 @@ func _on_BreakTimer_timeout():
 	_id -=1
 	if _id >= 0:
 		$BreakTimer.start()
-	else:
+	elif $Spirale/PathFollow2D/Title/TitleTimer != null:
 		$Spirale/PathFollow2D/Title/TitleTimer.start()
 
 func _piece_falls(id):
-	var piece : RigidBody2D = $Pieces.get_child(_id)
-	piece.mode = RigidBody2D.MODE_RIGID
-	var signe = randi() % 2 * 2 - 1
-	piece.add_force(Vector2(0, rand_range(500, 800)), Vector2(signe * rand_range(1000, 2000), 0))
+	if id >= 0 :
+		var piece : RigidBody2D = $Pieces.get_child(id)
+		piece.mode = RigidBody2D.MODE_RIGID
+		var signe = randi() % 2 * 2 - 1
+		piece.add_force(Vector2(0, rand_range(500, 800)), Vector2(signe * rand_range(1000, 2000), 0))
 
 
 func _on_TitleTimer_timeout():
